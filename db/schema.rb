89b1f7c94832acd4201659fc12a123fa83df6d68
lvisentin <lvise.batista@gmail.com>
2024-02-08 21:08:36 -0300

    ^

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_06_233735) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_08_124332) do
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "status"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.integer "article_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "status"
+    t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.text "title"
     t.text "body"
@@ -20,4 +38,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_06_233735) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "articles"
 end
